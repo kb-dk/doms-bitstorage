61adf1e3679291551f7ec6a64119dd5d3df98f08
Esben Agerbaek Black <eab@statsbiblioteket.dk>
2010-04-13 10:51:51 +0000

    Code matured to a point where integration with testbed & initialization of class is needed. Code is further prepared for changes to the Characterisation class, the plan is to let the changes facilitate the inclution of tool<->result mapping data

--- modules/characteriser/characteriser_impl/src/dk/statsbiblioteket/doms/bitstorage/characteriser/frontend/CharacteriseSoapWebserviceImpl.java
+++ modules/characteriser/characteriser_impl/src/dk/statsbiblioteket/doms/bitstorage/characteriser/frontend/CharacteriseSoapWebserviceImpl.java
@@ -27,6 +27,7 @@
 
 package dk.statsbiblioteket.doms.bitstorage.characteriser.frontend;
 
+import dk.statsbiblioteket.util.qa.QAInfo;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -36,6 +37,7 @@ import dk.statsbiblioteket.doms.bitstorage.characteriser.CommunicationException;
 import dk.statsbiblioteket.doms.bitstorage.characteriser.FileNotAvailableException;
 import dk.statsbiblioteket.doms.webservices.ConfigCollection;
 
+
 import javax.jws.WebParam;
 import javax.jws.WebService;
 
@@ -43,9 +45,8 @@ import javax.jws.WebService;
 import eu.planets_project.services.identify.Identify;
 import eu.planets_project.services.identify.IdentifyResult;
 import eu.planets_project.services.datatypes.DigitalObject;
+import eu.planets_project.services.datatypes.ServiceDescription;
 import eu.planets_project.ifr.core.storage.api.DigitalObjectManager;
-import eu.planets_project.fedora.FedoraObjectManager;
-import eu.planets_project.fedora.connector.FedoraConnectionException;
 
 
 import java.net.URI;
@@ -53,66 +54,46 @@ import java.net.URISyntaxException;
 import java.util.*;
 
 /**
- * Created by IntelliJ IDEA.
- * User: abr
- * Date: Dec 1, 2009
- * Time: 2:39:13 PM
- * To change this template use File | Settings | File Templates.
+ *
  */
+@QAInfo(author = "eab", reviewers = "", level = QAInfo.Level.NORMAL,
+        state = QAInfo.State.QA_OK)
 @WebService(endpointInterface = "dk.statsbiblioteket.doms.bitstorage.characteriser.CharacteriseSoapWebservice")
 public class CharacteriseSoapWebserviceImpl implements
         CharacteriseSoapWebservice {
-    /**
-     * The log for this class.
-     */
-    Log log = LogFactory.getLog(getClass());
-/*    String fedoraUrl = "";
-    String fedoraUser = "";
-    String fedoraUserPassword = "";
-*/
-    private Identify identifier;
 
+    Log log = LogFactory.getLog(getClass());    // Log for the class
     private DigitalObjectManager fedora;
-
-    private List<Identify> identifiers; //TODO get identifiers from config file. Perhaps in the Initialization?
-
+    private Map<IdentifyResult.Method, List<Identify>> identificationMethods; //TODO get identificationMethods from config file. Perhaps in the Initialization?
     private boolean initialised = false;
 
-//    @PostConstruct
 
-    //>>>>>>> .r190
     private void initialise() {
         if (initialised) {
             return;
         }
-//<<<<<<< .mine
-//        ServletContext servletContext =
-//                (ServletContext) webServiceContext.getMessageContext().get(
-//                        MessageContext.SERVLET_CONTEXT);
-//        String testparam = servletContext.getInitParameter("testParam");
-//        initialised = true;
-//
-//        Service service = Service.create(wsdlLocation, interfaceName);
-//        /* Enable streaming, if supported by the service: */
-//        @SuppressWarnings("unchecked") T ids = (T) service.getPort(implementationClass.getInterfaces()[0],
-//                new MTOMFeature());
-//        ((BindingProvider) ids).getRequestContext().put(JAXWSProperties.HTTP_CLIENT_STREAMING_CHUNK_SIZE, 8096);
-//        SOAPBinding binding = (SOAPBinding) ((BindingProvider) ids).getBinding();
-//        binding.setMTOMEnabled(true);
-//
-
-
-//=======
-
+        
+        // TODO actual implementation of getting setup and tools.
         String testparam = ConfigCollection.getProperties().getProperty("testParam");
-        initialised = true;
 
-//>>>>>>> .r190
+        initialised = true;
     }
 
     /**
+     * Retrieve an object and use all available tools in order to identify it.
+     * This method degrades on which identification method is used, the
+     * following is the ordering that is used, the best method with a result has
+     * its result returned.
      *
-     * @param pid
+     * The order of identification methods are:
+     * 1)  Full Parse
+     * 2)  Partial Parse
+     * 3)  Magic
+     * 4)  Metadata
+     * 5)  Extension
+     * 6)  Other 
+     *  
+     * @param pid of the object being identified
      * @return the characterisation of the object pertaining to the PID
      * @throws CommunicationException
      * @throws FileNotAvailableException if there is no file relating to the PID
@@ -126,108 +107,108 @@ public class CharacteriseSoapWebserviceImpl implements
         initialise();
 
         Characterisation returnCharacterisation = new Characterisation();
-        returnCharacterisation.setMd5CheckSum(""); // TODO Explain why; because it's not needed.
+        returnCharacterisation.setMd5CheckSum(""); // MD5sum is not needed.
         DigitalObject studiedObject = null;
         URI uri = null;
-        List<IdentifyResult> results = new ArrayList<IdentifyResult>();
+
         try {
             uri = new URI(pid);
             studiedObject = fedora.retrieve(uri);
         } catch (URISyntaxException e) {
-            String message = "URISyntax failex with exception: "+e;
+            String message = "URISyntax failex with exception: " + e;
             log.error(message, e);
             // TODO Error Handling
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         } catch (DigitalObjectManager.DigitalObjectNotFoundException e) {
-            String message = "Digital object not found: "+e;
+            String message = "Digital object not found: " + e;
             log.error(message, e);
             // TODO Error Handling
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
 
-        for (Identify identifier : identifiers){
-                results.add(identifier.identify(studiedObject, null));
-        }
+        Map<ServiceDescription, IdentifyResult> identificationResult;
+        identificationResult = new HashMap();
+        Map<IdentifyResult.Method, Map<ServiceDescription, IdentifyResult>>
+                identificationMethodResult = new HashMap();
+        Map<IdentifyResult.Method, List<URI>> resultMap = new HashMap();
+        Collection methods = identificationMethods.keySet();
 
-        Map<IdentifyResult.Method, List<IdentifyResult>> resultMap = null;
-        for (IdentifyResult identifiedResult : results){
-            resultMap.put(identifiedResult.getMethod(), results);
-        }
+        for (Object method : methods) {
+            List<Identify> identificationTools
+                    = identificationMethods.get((IdentifyResult.Method) method);
 
-
-
-//TODO        results = identifiedResult
-
-        for (IdentifyResult result: results){
-            List<IdentifyResult> item = resultMap.get(result.getMethod());
-            item.add(result);
-        }
-        Set<URI> pronomIDs = new HashSet<URI>();
-        List<IdentifyResult> full_list = resultMap.get(IdentifyResult.Method.FULL_PARSE);
-        List<IdentifyResult> partial_list = resultMap.get(IdentifyResult.Method.PARTIAL_PARSE);
-        List<IdentifyResult> magic_list = resultMap.get(IdentifyResult.Method.MAGIC);
-        List<IdentifyResult> meta_list = resultMap.get(IdentifyResult.Method.METADATA);
-        List<IdentifyResult> ext_list = resultMap.get(IdentifyResult.Method.EXTENSION);
-        if(full_list.size() > 0){
-
-            for(IdentifyResult result: full_list){
-                pronomIDs.addAll(result.getTypes());
+            for (Identify identifier : identificationTools) {
+                IdentifyResult objectIdentity
+                        = identifier.identify(studiedObject, null);
+                ServiceDescription toolDescription = identifier.describe();
+                identificationResult.put(toolDescription,
+                        objectIdentity);
+                resultMap.put(objectIdentity.getMethod(),
+                              objectIdentity.getTypes());
             }
-        }else if (partial_list.size() > 0){
-
-            for(IdentifyResult result: partial_list){
-                pronomIDs.addAll(result.getTypes());
-            }
-        }else if(magic_list.size() > 0){
-
-            for(IdentifyResult result: magic_list){
-                pronomIDs.addAll(result.getTypes());
-            }
-        }else if(meta_list.size() > 0){
-
-            for(IdentifyResult result: meta_list){
-                pronomIDs.addAll(result.getTypes());
-            }
-        }else if(ext_list.size() > 0){
-
-            for(IdentifyResult result: ext_list){
-                pronomIDs.addAll(result.getTypes());
-            }
+            identificationMethodResult.put((IdentifyResult.Method) method,
+                    identificationResult);
         }
-        if(pronomIDs.size()<1){
-            //here there be dragons
-            //file is not identified
-        }
-        //Characterisation characterization = new Characterisation();
-        List<String> characterization = outPutListAsStrings(pronomIDs);
 
-
-        //characterise()
-        returnCharacterisation.getPronomID().addAll((Collection) pronomIDs);
-        if (returnCharacterisation.getPronomID().size() > 0){
-            // TODO omskriv denne OG characterization til at have en fornuftig validationStatus evt til en boolean og en liste af valideringer
-            returnCharacterisation.setValidationStatus("Validated");
+        List<String> pronomIDs;
+        String validationStatus;
+        /**
+         * Use the higest valued method, returning results, to set the values
+         * that make up the returnCharacterisation values.
+         */
+        if (resultMap.get(IdentifyResult.Method.FULL_PARSE).size()>0){
+            pronomIDs = convertURI(resultMap.get(
+                    IdentifyResult.Method.FULL_PARSE));
+            validationStatus = "FULL_PARSE";
+            identificationResult = identificationMethodResult.get(
+                    IdentifyResult.Method.FULL_PARSE);
+        } else if (resultMap.get(IdentifyResult.Method.PARTIAL_PARSE).size()>0){
+            pronomIDs = convertURI(resultMap.get(
+                    IdentifyResult.Method.PARTIAL_PARSE));
+            validationStatus = "PARTIAL_PARSE";
+            identificationResult = identificationMethodResult.get(
+                    IdentifyResult.Method.PARTIAL_PARSE);
+        } else if (resultMap.get(IdentifyResult.Method.MAGIC).size()>0){
+            pronomIDs = convertURI(resultMap.get(IdentifyResult.Method.MAGIC));
+            validationStatus = "MAGIC";
+            identificationResult = identificationMethodResult.get(
+                    IdentifyResult.Method.MAGIC);
+        } else if (resultMap.get(IdentifyResult.Method.METADATA).size()>0){
+            pronomIDs = convertURI(resultMap.get(
+                    IdentifyResult.Method.METADATA));
+            validationStatus = "METADATA";
+            identificationResult = identificationMethodResult.get(
+                    IdentifyResult.Method.METADATA);
+        } else if (resultMap.get(IdentifyResult.Method.EXTENSION).size()>0){
+            pronomIDs = convertURI(resultMap.get(
+                    IdentifyResult.Method.EXTENSION));
+            validationStatus = "EXTENSION";
+            identificationResult = identificationMethodResult.get(
+                    IdentifyResult.Method.EXTENSION);
+        } else if (resultMap.get(IdentifyResult.Method.OTHER).size()>0){
+            pronomIDs = convertURI(resultMap.get(IdentifyResult.Method.OTHER));
+            validationStatus = "OTHER";
+            identificationResult = identificationMethodResult.get(
+                    IdentifyResult.Method.OTHER);
+        } else {
+            pronomIDs = null;
+            validationStatus = "VALIDATION_FAILED"; // TODO verify that this will at the very least throw an error
+            identificationResult = null;
         }
-        return returnCharacterisation;  //To change body of implemented methods use File | Settings | File Templates.
+        
+        returnCharacterisation.getPronomID().addAll(pronomIDs);
+        returnCharacterisation.setValidationStatus(validationStatus);
+// TODO adapt Characterisation.java to reflect below line 
+//        returnCharacterisation.getIdentificationToolResults().putAll(identificationResult);
+        return returnCharacterisation;
     }
 
-/*
-
-    private List<String> outPutListAsStrings(Characterisation list) {
-        ArrayList<String> outputList = new ArrayList<String>();
-        for(Object o:list){
-            outputList.add(o.toString());
+    private List<String> convertURI(Collection<URI> uris) {
+        List<String> retList = new ArrayList<String>();
+        for (URI tmpUri : uris){
+            retList.add(tmpUri.toString());
         }
-        return outputList;
+        return retList;
     }
-*/
 
-
-    private List<String> outPutListAsStrings(Set<URI> inputList) {
-        ArrayList<String> outputList = new ArrayList<String>();
-        for (Object o : inputList) {
-            outputList.add(o.toString());
-        }
-        return outputList;
-    }
 }
